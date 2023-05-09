3610ec192a4786efd46cb5e22d9dee9ef28614e6
Robert Grimm <apparebit@gmail.com>
2023-05-09 12:53:42 -0400

    Add macros to nicely format emoji inventory; improve build script

--- build.sh
+++ build.sh
@@ -19,11 +19,6 @@ log() {
     printf "${STYLE}$1 $2 ${RESET}\n" >&2
 }
 
-nope() {
-    log ERROR "$@"
-    exit 1
-}
-
 if [[ -z ${BASH} ]]; then
     log ERROR "It looks like you source'd this script; please run it instead"
     log STUFF '$ chmod +x build.sh'
@@ -31,33 +26,63 @@ if [[ -z ${BASH} ]]; then
     return 1
 fi
 
+test-with-engine() {
+    log INFO "Test with $1"
+    "$1" -jobname=$1-canary -interaction=batchmode canary
+    if [ $? -ne 0 ]; then
+        log ERROR "$1 failed to compile 'canary.tex'"
+        exit 1
+    fi
+}
+
 test() {
-    pdflatex -jobname=pdftex-canary canary   || nope "pdflatex failed"
-    xelatex -jobname=xetex-canary canary     || nope "xelatex failed"
-    lualatex -jobname=luatex-canary canary   || nope "lualatex failed"
-    pdfunite pdftex-canary.pdf xetex-canary.pdf luatex-canary.pdf canary.pdf
+    test-with-engine pdflatex
+    test-with-engine xelatex
+    test-with-engine lualatex
+    pdfunite pdflatex-canary.pdf xelatex-canary.pdf lualatex-canary.pdf canary.pdf
     log INFO 'Be sure to inspect "canary.pdf"!'
 }
 
 html() {
+    log INFO "Test with LaTeXML"
     # LaTeXML: --includestyles handles emo-supprt package
-    latexmlc --includestyles --dest=demo-latexml.html demo.tex  || nope "LaTeXML failed"
+    latexmlc --includestyles --dest=demo-latexml.html demo.tex
+    if [ $? -ne 0 ]; then
+        log ERROR "latexml failed to convert 'demo.tex' to HTML"
+        exit 1
+    fi
 
     # Remove run date comment and footer. The latter is just plain tacky.
     sed -i '' '/^<!--Generated on /d' ./demo-latexml.html
     sed -i '' '/^<footer class="ltx_page_footer">/{N;N;d;}' ./demo-latexml.html
 
+    log INFO "Test with TeX4ht"
     # TeX4ht: -l is necessary for selecting LuaTeX engine
-    make4ht -l -j demo-tex4ht demo.tex  || nope "TeX4ht failed"
+    make4ht -l -j demo-tex4ht demo.tex
+    if [ $? -ne 0 ]; then
+        log ERROR "tex4ht failed to convert 'demo.tex' to HTML"
+        exit 1
+    fi
+
     log INFO 'Be sure to inspect "demo.html"!'
 }
 
+build-docs() {
+    log INFO "Build documentation"
+    lualatex -interaction=batchmode emo.dtx
+    if [ $? -ne 0 ]; then
+        log ERROR "lualatex failed to compile 'emo.dtx'"
+        exit 1
+    fi
+}
+
 docs() {
-    lualatex emo.dtx
+    build-docs
+    log INFO "Make indices"
     makeindex -s gind.ist -o emo.ind emo.idx
     makeindex -s gglo.ist -o emo.gls emo.glo
-    lualatex emo.dtx
-    lualatex emo.dtx
+    build-docs
+    build-docs
 }
 
 target=${1:-all}
